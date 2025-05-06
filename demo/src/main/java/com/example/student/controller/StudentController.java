3ee5291ecd07b97b6fc28c62217e65e21c594069
Yuelf12345 <2794127100@qq.com>
2025-05-06 11:25:08 +0800

    controller

--- demo/src/main/java/com/example/student/controller/StudentController.java
+++ demo/src/main/java/com/example/student/controller/StudentController.java
@@ -1,4 +1,26 @@
 package com.example.student.controller;
 
+import com.example.student.entity.Student;
+import com.example.student.service.StudentService;
+import com.example.student.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    /**
+     * 查询学生列表信息
+     * @return Result
+     */
+    @GetMapping("/list")
+    public Result<List<Student>> list() {
+        return Result.success(studentService.list());
+    }
 }
